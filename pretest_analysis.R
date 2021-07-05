a093b8c236f72b528c7e4ac0865b52b259f075b5
Pascal Gelfort <pascalgelfort1998@googlemail.com>
2021-07-05 21:51:03 +0200

    Scripts

---
+++ pretest_analysis.R
@@ -0,0 +1,142 @@
+setwd("C:/Users/pasca/Desktop/MASTERARBEIT/githubtest/myrepo")
+load(file = "pretest23062021.Rda")
+
+#### Packages ####
+library(psych)
+library(tidyr)
+library(readxl)
+library(lme4) 
+library(lmerTest)
+library(nlme)
+library(performance)
+library(lattice)
+library(see)
+#### Legende ####
+# be/st/ch/vis am Ende von dataframes etc stehen f?r belief/status/choice of membership/visibility
+# mask/gen am Ende von dataframes etc stehen f?r generisches Maskulin/gendersternchen
+
+
+
+###############################################################
+################## Datenaufbereitung ##########################
+###############################################################
+
+d<-ds_4[6:483]
+d$ID<-c(1:180)
+describe(d$DE02_01)
+
+belief<-d[1:116]
+status<-d[117:232]
+choice<-d[233:348]
+vis<-d[349:464]
+
+#### NAs z?hlen ####
+#pro Dimension und grammat. Geschlecht
+beliefmask<-belief[1:58]
+beliefmaskna<-apply(beliefmask, MARGIN = 1, function(x) sum(is.na(x)))
+beliefgen<-belief[59:116]
+beliefgenna<-apply(beliefgen, MARGIN = 1, function(x) sum(is.na(x)))
+
+vismask<-vis[1:58]
+vismaskna<-apply(vismask, MARGIN = 1, function(x) sum(is.na(x)))
+visgen<-vis[59:116]
+visgenna<-apply(visgen, MARGIN = 1, function(x) sum(is.na(x)))
+
+choicemask<-choice[1:58]
+choicemaskna<-apply(choicemask, MARGIN = 1, function(x) sum(is.na(x)))
+choicegen<-choice[59:116]
+choicegenna<-apply(choicegen, MARGIN = 1, function(x) sum(is.na(x)))
+
+statusmask<-status[1:58]
+statusmaskna<-apply(statusmask, MARGIN = 1, function(x) sum(is.na(x)))
+statusgen<-status[59:116]
+statusgenna<-apply(statusgen, MARGIN = 1, function(x) sum(is.na(x)))
+
+nacount<-cbind(beliefmaskna,beliefgenna,statusmaskna,statusgenna,choicemaskna,choicegenna,vismaskna,visgenna)
+nacount<-as.data.frame(cbind(nacount,d$FU03))
+
+#Grammatikvariable erzeugen 0=generisches Maskulin 1=gender*
+nacount$gen<-ifelse(nacount$V9>24,1,0)
+nacount$beliefna<-ifelse(nacount$gen==1,nacount$beliefgenna, nacount$beliefmaskna)
+nacount$statusna<-ifelse(nacount$gen==1,nacount$statusgenna, nacount$statusmaskna)
+nacount$choicena<-ifelse(nacount$gen==1,nacount$choicegenna, nacount$choicemaskna)
+nacount$visna<-ifelse(nacount$gen==1,nacount$visgenna, nacount$vismaskna)
+nacount$allna<-{nacount$beliefna+nacount$statusna+nacount$choicena+nacount$visna}
+
+table(nacount$allna)
+
+nacount[is.na(nacount)]<-""
+
+#### NAs entfernen ####
+d$nacount<-nacount$allna
+d<-subset(d,d$nacount==0)
+
+# 103 Datens?tze bleiben
+
+######191 Jahre Person#############
+d$DE02_01<-ifelse(d$DE02_01==191,NA,d$DE02_01)
+
+###############################################################
+############## Vorbereitung f?r Analysen ######################
+###############################################################
+
+##### Long format ####
+# alle Ratings in einer Spalte 
+# Spalten mit Faktoren Dimension, Targetgroups, grammat. Geschlecht etc
+# 232 Zeilen pro ID
+targetdiff<-read_xlsx("C:\\Users\\pasca\\Desktop\\MASTERARBEIT\\Pretest_R_final\\targetnames.xlsx")
+longdatmask<-cbind(d$ID,d$FU03,d$DE02_01, d$DE03, d$DE04, d$DE05_01,d$DE06,d[1:58],d[117:174],d[233:290],d[349:406])
+longdatmask<-gather(longdatmask,"target","rating",BE01_01:VI01_58)
+longdatmask<-subset(longdatmask,rating!="NA")
+longdatgen<-cbind(d$ID,d$FU03,d$DE02_01, d$DE03, d$DE04, d$DE05_01,d$DE06,d[59:116],d[175:232],d[291:348],d[407:464])
+longdatgen<-gather(longdatgen,"target","rating",BE02_01:VI02_58)
+longdatgen<-subset(longdatgen,rating!="NA")
+longdat<-rbind(longdatmask,longdatgen)
+longdat<-longdat[order(longdat$`d$ID`),]
+longdat$targetnames<-rep(targetdiff$target,4*103)
+longdat$dimension<-rep(c(rep("belief",58),rep("status",58),rep("choice",58),rep("visibility",58)),103)
+longdat$grammarcond<-ifelse(longdat$`d$FU03`>24,1,0)
+colnames(longdat)<-c("ID","order","age","gender","education","ideology",
+                     "job","target","rating","targetnames","dimension","grammarcond")
+
+#### Long dim ####
+# long Format nach Dimensionen aufgeiteilt
+# pro Dimension eine Spalte
+# 58 Zeilen pro ID
+longdim<-spread(longdat,dimension,rating)
+longdimbe<-subset(longdim,longdim$belief!="NA")
+#### Wert -9 durch NA ersetzen ####
+longdimbe$belief<-ifelse(longdimbe$belief==-9,NA,longdimbe$belief)
+longdimst<-subset(longdim,longdim$status!="NA")
+longdimch<-subset(longdim,longdim$choice!="NA")
+longdimvis<-subset(longdim,longdim$visibility!="NA")
+longdimbe$status<-longdimst$status
+longdimbe$choice<-longdimch$choice
+longdimbe$visibility<-longdimvis$visibility
+longdim<-longdimbe
+longdim$differentiation<-rep(targetdiff$differentiation,103)
+
+#### wide Format ####
+# wide Format mit Ratings pro Target
+# pro Dimension eine Zeile
+widedat<-pivot_wider(
+  longdim,
+  id_cols = ID,
+  names_from = targetnames,
+  names_prefix = "",
+  names_sep = "_",
+  names_glue = NULL,
+  names_sort = FALSE,
+  names_repair = "check_unique",
+  values_from = c(belief,status,choice,visibility),
+  values_fill = NULL,
+  values_fn = NULL
+)
+widedatdemographics<-longdat[,c(1,3,4,5,6,7,12)]
+widedatdemographics<-unique(widedatdemographics, by=ID)
+widedat<-cbind(widedat$ID,widedatdemographics[,2:7],widedat[2:233])
+
+#Rating -9 auch aus longdat entfernen
+longdat$rating<-ifelse(longdat$rating==-9,NA,longdat$rating)
+
+
---
+++ pretest_analysis2.R
@@ -0,0 +1,133 @@
+###############################################################
+######################## Analysen #############################
+###############################################################
+
+####Stichprobenbeschreibung####
+#Alter
+describe(d$DE02_01)
+#Gender
+table(d$DE03)
+#Bildung
+table(d$DE04)
+#Beruf
+table(d$DE06)
+#Ideologie
+describe(d$DE05_01)
+
+#### Deskriptive Daten pro Dimension und Targetgroup
+#Belief
+descbe<-by(longdim$belief,longdim$targetnames,describe)
+descbe<- as.data.frame(do.call(rbind,descbe))
+#Status
+descst<-by(longdim$status,longdim$targetnames,describe)
+descst<- as.data.frame(do.call(rbind,descst))
+#Choice
+descch<-by(longdim$choice,longdim$targetnames,describe)
+descch<- as.data.frame(do.call(rbind,descch))
+#Visibility
+descvis<-by(longdim$visibility,longdim$targetnames,describe)
+descvis<- as.data.frame(do.call(rbind,descvis))
+
+#### Korrelation der Dimensionen untereinander und mit Ideologie ####
+corideodim<-corr.test(longdim[,c(6,11:14)])
+
+#### Korrelation der Ratings mit Ideologie ####
+corideobe<-corr.test(widedat[,c(5,8:65)])
+corideost<-corr.test(widedat[,c(5,66:123)])
+corideovch<-corr.test(widedat[,c(5,124:181)])
+corideovis<-corr.test(widedat[,c(5,182:239)])
+
+####
+
+#### Grammar Condition ####
+#Korrelation
+longcormask<-subset(longdat, longdat$grammarcond==0)
+longcormask[11369:12528,1:12]<-NA
+longcorgen<- subset(longdat, longdat$grammarcond==1)
+gendercor<-corr.test(longcorgen$rating,longcormask$rating, use = "complete.obs")
+gendercor$ci
+#t test
+gramttest<-pairwise.t.test(longdat$rating,longdat$grammarcond)
+#loop
+n <- 239
+ttests<-matrix(NA,239,1)
+for(i in 8:n) {
+  ttests[i,]<-print(pairwise.t.test(widedat[,i],widedat$grammarcond))$p.value;
+}
+ttests<-as.data.frame(ttests)
+ttests$dimension_target<-colnames(widedat)
+ttests$sig<-ifelse(ttests$V1<0.05,
+                   ifelse(ttests$V1<0.01,
+                          ifelse(ttests$V1<0.001,"***","**"),"*"),"")
+ttests<-ttests[8:239,1:3]
+table(ttests$sig)
+#
+grammarmod4 <- lmer(rating ~ grammarcond * dimension + (1|targetnames)+ (1|ID) ,REML =FALSE,data = longdat)
+grammarmod3 <- lmer(rating ~ grammarcond + dimension + (1|targetnames)+ (1|ID) ,REML =FALSE,data = longdat)
+grammarmod2 <- lmer(rating ~ grammarcond + (1|targetnames)+ (1|ID) ,REML =FALSE,data = longdat)
+grammarmod1 <- lm(rating ~ grammarcond*dimension  ,data = longdat)
+grammarmod <- lm(rating ~ grammarcond  ,data = longdat)
+anova(grammarmod4,grammarmod3)
+compare_performance(grammarmod4,grammarmod3,grammarmod2,grammarmod1,grammarmod)
+plot(modperf)
+summary(grammarmod4)
+fixef(grammarmod4)
+ranef(grammarmod4)
+begram<-lmer(belief ~ grammarcond  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(begram)
+dotplot(ranef(begram))
+stgram<-lmer(status ~ grammarcond  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(stgram)
+dotplot(ranef(stgram))
+chgram<-lmer(choice ~ grammarcond  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(chgram)
+dotplot(ranef(chgram))
+visgram<-lmer(visibility ~ grammarcond  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(visgram)
+dotplot(ranef(visgram))
+#### Zero Models ####
+#belief
+bezmod<-lmer(belief ~ 1  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(bezmod)
+dotplot(ranef(bezmod))
+#status
+stzmod<-lmer(status ~ 1  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+summary(stzmod)
+#choice
+chzmod<-lmer(choice ~ 1  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+#visibility 
+viszmod<-lmer(visibility ~ 1  + (1|targetnames) + (1|ID),REML =FALSE, data = longdim)
+dotplot(ranef(stzmod))
+dotplot(ranef(chzmod))
+dotplot(ranef(viszmod))
+summary(viszmod)
+#### Zusammenh?nge Dimensionen ####
+bestmod <- lmer(belief ~ status  + (status|targetnames) + (status|ID),REML =FALSE,data = longdim)
+dotplot(ranef(bestmod))
+summary(bestmod)
+check_model(bestmod)
+performance(bestmod)
+bechmod <- lmer(belief ~ choice  + (choice|targetnames) + (choice|ID),REML =FALSE,data = longdim)
+summary(bechmod)
+bevismod <- lmer(belief ~ visibility  + (visibility|targetnames) + (visibility|ID),REML =FALSE,data = longdim)
+summary(bevismod)
+stchmod <- lmer(status ~ choice  + (choice|targetnames) + (choice|ID),REML =FALSE,data = longdim)
+summary(lm(status~choice,longdim))
+summary(stchmod)
+performance(stchmod)
+fixef(stchmod)
+ranef(stchmod)
+stvismod <- lmer(status ~ visibility  + (visibility|targetnames) + (visibility|ID),REML =FALSE,data = longdim)
+confint.merMod(stchmod)
+summary(stvismod)
+dotplot(ranef(stvismod))
+chvismod <- lmer(choice ~ visibility  + (visibility|targetnames) + (visibility|ID),REML =FALSE,data = longdim)
+summary(chvismod)
+plot(ranef.mer(chvismod))
+performance(lm(status~choice,data = longdim))
+nlme::intervals.lme(stvismod)
+ranstch<-ranef(stchmod)
+confint(stchmod, oldNames=FALSE)
+
+
+by(longdim$belief,longdim$ID,sd)
